211f22241102de74219fe326e1c28686ecf7c8da
Ryan Kjesbo-Johnson <Ryanmark86@gmail.com>
2023-12-20 11:13:20 -0700

    Completed dog post route

--- server/routes/dogs.router.js
+++ server/routes/dogs.router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+//This route gets all dogs from the database table dog_profiles
 router.get('/', (req, res) => {
   const query = `
   SELECT * FROM "dog_profiles";
@@ -16,11 +17,30 @@ pool.query(query)
   })
 });
 
-/**
- * POST route template
- */
+//This is the post router to create a new dog
 router.post('/', (req, res) => {
-  // POST route code here
+  console.log(req.body);
+  const insertNewDogQuery = `
+    INSERT INTO "dog_profiles" 
+    ("dog_name", "dog_sh_descr", "dog_lg_descr", "pic_url")
+      VALUES
+      ($1, $2, $3, $4);
+  `;
+  const insertNewDogValues = [
+    req.body.dog_name,
+    req.body.dog_sh_descr,
+    req.body.dog_lg_descr,
+    req.body.dog_url
+]
+  pool.query(insertNewDogQuery, insertNewDogValues)
+    .then(result => {
+      console.log('New Dog created')
+      res.sendStatus(201)
+    })
+    .catch(err => {
+        console.log(err);
+        res.sendStatus(500)
+    })
 });
 
 module.exports = router;
