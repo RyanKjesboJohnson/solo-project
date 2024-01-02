66b1f5db88adf370cc6fdfb31be1753cba2f33de
Ryan Kjesbo-Johnson <Ryanmark86@gmail.com>
2024-01-02 16:17:41 -0600

    Small updates to the add dog form

--- src/components/AddDog/AddDog.jsx
+++ src/components/AddDog/AddDog.jsx
@@ -1,4 +1,4 @@
-import { Container, Grid, TextField, Typography, Box, FormControl, OutlinedInput } from "@mui/material";
+import { Container, Grid, TextField, Typography, Box, FormControl, OutlinedInput, colors } from "@mui/material";
 import React, { useState } from "react";
 
 export default function AddDog() {
@@ -13,8 +13,8 @@ export default function AddDog() {
             <Box sx={{m:5, alignContent:"center"}}>
             <Typography variant="h3">Add New Dog</Typography>
             </Box>
-        <Grid container direction="row" justify="flex-start" alignItems="flex-start" spacing={1}>
-            <Grid container xs="12" spacing="1">
+        <Grid container direction="row" justify="flex-start" alignItems="flex-start" width='175ch' spacing={0} margin={10} sx={{border: 1, borderColor: 'grey.500'}}>
+            <Grid container xs="12" spacing="1" margin={10}>
                 <Grid item xs={12} sm={6}>
                     <FormControl>
                         <OutlinedInput
@@ -23,17 +23,47 @@ export default function AddDog() {
                         value={dogNameInput}
                         placeholder="Enter Dog's Name"
                         onChange={(event) => setDogNameInput(event.target.value)}
+                        style={{width:500}}
                         />
                     </FormControl>
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <FormControl fullWidth>
+                    <FormControl>
                         <OutlinedInput
                         id="dogShDesc"
                         label="Dog's Short Description"
                         value={dogShDescInput}
                         placeholder="Enter Dog's Short Description"
                         onChange={(event) => setDogShDescInput(event.target.value)}
+                        style={{width:700}}
+                        />
+                    </FormControl>
+                </Grid>
+            </Grid>
+            <Grid container xs="12" spacing="1" margin={10}>
+                <Grid item xs={12} sm={9}>
+                    <FormControl>
+                        <OutlinedInput
+                        id="dogLgDesc"
+                        label="Dog's Long Description"
+                        multiline
+                        value={dogLgDescInput}
+                        placeholder="Enter Dog's Long Description"
+                        onChange={(event) => setDogLgDescInput(event.target.value)}
+                        style={{width:800}}
+                        rows={6}
+                        />
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} sm={3}>
+                    <FormControl>
+                        <OutlinedInput
+                        id="dogShDesc"
+                        label="Dog's Short Description"
+                        value={dogShDescInput}
+                        placeholder="Enter Dog's Short Description"
+                        onChange={(event) => setDogShDescInput(event.target.value)}
+                        style={{width:300}}
                         />
                     </FormControl>
                 </Grid>
