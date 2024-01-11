62383b131fa0e5643f40e050ea58477ec4dd1d45
Ryan Kjesbo-Johnson <Ryanmark86@gmail.com>
2024-01-11 10:27:15 -0600

    tested swiper click action to trigger log

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -2,35 +2,48 @@ import React, { useState, useEffect, useRef } from "react";
 import { useHistory } from "react-router-dom";
 import "./LandingPage.css";
 import { Container, Typography } from "@mui/material";
-import Swiper from "swiper/bundle";
 import { register } from "swiper/element/bundle";
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
 
-register();
 
 //The landing page functions as the first page that guests and users see
 //When a guest clicks meet our dogs button they are navigated to the dogprofiles page
 function LandingPage() {
   // const [heading, setHeading] = useState("All Dog Rescue");
+  const swiperRef = useRef(null);
   const history = useHistory();
-  const swiper = new Swiper('.swiper', {
-    speed: 400,
-    on: {
-      init: function () {console.log('swiper initialized');}
-    }
-  })
-  // const swiperElRef = useRef(null);
+  
+  useEffect(() => {
+    // Register Swiper web component
+    register();
 
-  // useEffect(() => {
-  //   // listen for Swiper events using addEventListener
-  //   swiperElRef.current.addEventListener('swiperprogress', (e) => {
-  //     const [swiper, progress] = e.detail;
-  //     console.log(progress);
-  //   });
+    // Add event listener
+    swiperRef.current.addEventListener('swiperslidechange', (e) => {
+      console.log(e.detail);
+    });
 
-  //   swiperElRef.current.addEventListener('swiperslidechange', (e) => {
-  //     console.log('slide changed');
-  //   });
-  // }, []);
+    // Object with parameters
+    const params = {
+      // or pass it in on
+      navigation: true,
+      pagination: true,
+      slidesPerView: 1,
+      on: {
+        click(s) {
+          console.log(s);
+        },
+      },
+      
+    };
+
+    // Assign it to swiper element
+    Object.assign(swiperRef.current, params);
+
+    // initialize swiper
+    swiperRef.current.initialize();
+  }, []);
+
 
   const onLogin = (event) => {
     history.push("/login");
@@ -43,22 +56,17 @@ function LandingPage() {
   return (
     <Container fixed>
       <Typography variant="h2"></Typography>
-      {/* <div className="container">
-        <img src="/images/milo_photo.jpeg" height={900} />
-        <button onClick={meetOurDogs}>Meet Our Dogs</button>
-      </div> */}
 
-      <swiper-container navigation="true" pagination="true" data-swiper-autoplay="4000">
+      <swiper-container init="false" ref={swiperRef}>
         <swiper-slide>
-          <div className="container">
+
             <img src="/images/milo_photo.jpeg" />
             <button onClick={meetOurDogs}>Meet Our Dogs</button>
-          </div>
+
         </swiper-slide>
         <swiper-slide>
           <div className="container">
             <img src="/images/All_Dog_Logo.jpg" />
-            <button onClick={meetOurDogs}>Meet Our Dogs</button>
           </div>
         </swiper-slide>
       </swiper-container>
